5b065c7064c4d1cef51db69a82603059e3038ccf
anna.bielskaya <anna.bielskaya@gmail.com>
2026-04-19 13:03:09 +0300

    Removed unnecessary imports

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,5 +1,3 @@
-import React, { useState } from "react";
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Log = ({ gameTurns }) => {
   return (
     <ol id="log">
